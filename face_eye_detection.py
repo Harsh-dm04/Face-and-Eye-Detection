d0b20ad76ef502a57ff02616eee43a04f4e99b8c
Harsh-dm04 <166389458+Harsh-dm04@users.noreply.github.com>
2025-01-15 19:09:00 +0530

    face_eye_detection.py

--- face_eye_detection.py
+++ face_eye_detection.py
@@ -1,24 +1,3 @@
-# import cv2
-
-# # Load the Haar Cascade file for object detection (e.g., face detection)
-# cascade_path = "haarcascade_frontalface_default.xml"  # Replace with your Haar cascade file
-# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + cascade_path)
-
-# # Load the image
-# img = cv2.imread("demo_img.jpeg")
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# # Detect objects (e.g., faces)
-# faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-# # Draw rectangles around detected objects
-# for (x, y, w, h) in faces:
-#     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-# # Display the result
-# cv2.imshow("Detected Objects", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 
 import cv2
 import numpy as np
@@ -27,9 +6,9 @@ cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml") 
 
-# # if not cap.isOpened():
-# #     print("Error opening video stream or file")
-# #     exit()
+if not cap.isOpened():
+    print("Error opening video stream or file")
+    exit()
 
 while True:
     ret, frame = cap.read()
